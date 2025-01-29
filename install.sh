a26fb66e301855feb074604e54a9fa5c2f129c3b
lars20070 <l.nilse@dunelm.org.uk>
2025-01-29 07:48:20 +0100

    add Tavily API key

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-# Install dependencies
-virtualenv -p python3 ./.venv
+# # Install dependencies
+# virtualenv -p python3 ./.venv
+# # shellcheck source=/dev/null
+# . ./.venv/bin/activate
+# pip install --upgrade pip
+# pip list
+# deactivate
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  # shellcheck source=/dev/null
+  . .env
+fi
+
+# Run the script
 # shellcheck source=/dev/null
 . ./.venv/bin/activate
-pip install --upgrade pip
-pip list
+uvx --version
+echo "$TAVILY_API_KEY"
 deactivate
-
-# # Run the script
-# . ./.venv/bin/activate
-# uvx --version
-# deactivate
